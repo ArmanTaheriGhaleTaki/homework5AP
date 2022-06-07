f0507a9976104c039a1a794ab9bb8e47278b9144
Arman Taheri <armantahery1381@gmail.com>
2022-06-07 21:47:24 +0430

    fixing map bug

--- Professor.h
+++ Professor.h
@@ -4,16 +4,18 @@
 
 #include "Person.h"
 #include <map>
-std::map<std::string , int >ranks={
-        {"Instructor",1},
-        {"Professor Assistant",2},
-        {"Associate Professor",3},
-        {"Professor",4}
-};
+
 class Professor : public Person {
 private:
     std::string title;
 public:
+    std::map<std::string, int> ranks = {
+            {"Instructor",          1},
+            {"Professor Assistant", 2},
+            {"Associate Professor", 3},
+            {"Professor",           4}
+    };
+
     Professor();
 
     virtual bool validate(std::string _id);
